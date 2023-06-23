3d43b15d822d9ea5ad25e7d42afd3a59a3e1e445
mrympj <pirjamaat2001@gmail.com>
2023-06-23 15:56:20 -0400

    Code optimization

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_COMMAND_LENGTH 100
+#define MAX_COMMAND_LENGTH 1000
 # define MAX_ARGS 10
 
 void execute_ssh(char * args[]) {
@@ -15,6 +15,14 @@ void execute_ssh(char * args[]) {
         exit(1);
     } else {
         wait(NULL);
+    }
+}
+
+void execute_command_in_background(char * args[]) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        execvp(args[0], args);
+        exit(1);
     }
 }
 
@@ -28,7 +36,7 @@ void execute_sudo_apt_get(char * args[]) {
     }
 }
 
-int execute_command(char * command) {
+void execute_command(char * command) {
     char * args[MAX_ARGS];
     int argc = 0;
 
@@ -44,241 +52,35 @@ int execute_command(char * command) {
 
     if (argc == 0) {
         // No command entered
-        return 0;
+        return;
     }
 
-    if (strcmp(args[0], "ssh") == 0) {
+    if (strcmp(args[0], "&") == 0) {
+        argc--;
+        args[argc] = NULL;
+        execute_command_in_background(args);
+    } else if (strcmp(args[0], "ssh") == 0) {
         execute_ssh(args);
-    }
-
-    if (strcmp(args[0], "sudo") == 0 && strcmp(args[1], "apt-get") == 0) {
+    } else if (strcmp(args[0], "sudo") == 0 && strcmp(args[1], "apt-get") == 0) {
         execute_sudo_apt_get(args);
-    } else if (strcmp(args[0], "ls") == 0) {
-        // Implementation ls
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("ls", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "mkdir") == 0) {
-        // Implementation mkdir
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("mkdir", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "rm") == 0) {
-        // Implementation rm
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("rm", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "cat") == 0) {
-        // Implementation cat
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("cat", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "cp") == 0) {
-        // Implementation cp
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("cp", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "cd") == 0) {
-        // Implementation cd
-        if (argc > 1) {
-            if (chdir(args[1]) != 0) {
-                printf("cd: No such directory: %s\n", args[1]);
-            }
-        } else {
-            printf("cd: Missing directory name\n");
-        }
-    } else if (strcmp(args[0], "pwd") == 0) {
-        // Implementation pwd
-        char cwd[1024];
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            printf("Current directory: %s\n", cwd);
-        } else {
-            printf("Error getting current directory\n");
-        }
-    } else if (strcmp(args[0], "echo") == 0) {
-        // Implementation echo
-        for (int i = 1; i < argc; i++) {
-            printf("%s ", args[i]);
-        }
-        printf("\n");
-    } else if (strcmp(args[0], "wget") == 0) {
-        // Implementation wget
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("wget", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "head") == 0) {
-        // Implementation head
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("head", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "tail") == 0) {
-        // Implementation tail
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("tail", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "scp") == 0) {
-        // Implementation scp
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("scp", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "find") == 0) {
-        // Implementation find
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("find", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "ping") == 0) {
-        // Implementation ping
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("ping", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "top") == 0) {
-        // Implementation top
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("top", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "rpm") == 0) {
-        // Implementation rpm
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("rpm", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "mv") == 0) {
-        // Implementation mv
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("mv", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "grep") == 0) {
-        // Implementation grep
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("grep", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "chmod") == 0) {
-        // Implementation chmod
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("chmod", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "chown") == 0) {
-        // Implementation chown
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("chown", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "touch") == 0) {
-        // Implementation touch
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("touch", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "tar") == 0) {
-        // Implementation tar
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("tar", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "gzip") == 0) {
-        // Implementation gzip
-        pid_t pid = fork();
-        if (pid == 0) {
-            execvp("gzip", args);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else if (strcmp(args[0], "exit") == 0) {
-        // Terminate the program
-        exit(0);
     } else {
-        // Invalid command
-
-        printf("Invalid command: %s\n", args[0]);
+        // Execute the command
+        pid_t pid = fork();
+        if (pid == 0) {
+            execvp(args[0], args);
+            exit(1);
+        } else {
+            wait(NULL);
+        }
     }
-
-    return 1;
 }
-
 int main() {
     char command[MAX_COMMAND_LENGTH];
 
     while (1) {
-        printf("Enter a command: ");
+        printf(">> ");
         fgets(command, MAX_COMMAND_LENGTH, stdin);
-
-        size_t command_length = strlen(command);
-        if (command[command_length - 1] == '\n') {
-            command[command_length - 1] = '\0';
-        }
+        command[strcspn(command, "\n")] = '\0';
 
         if (strcmp(command, "exit") == 0) {
             break;
